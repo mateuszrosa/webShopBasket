e2e2017e338d560fb7a2667250ac28fcb4a2398a
mateuszrosa <mateusz.rosa@gmail.com>
2020-06-28 22:03:35 +0200

    span with final price

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inputs = document.querySelectorAll('input[type="number"]');
+const sum = document.querySelector("span.font-bold");
 
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("change", (e) => {
-    console.log(e.target);
+    console.log(sum.textContent);
   });
 }
