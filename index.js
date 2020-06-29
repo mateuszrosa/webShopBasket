f680b4c966532233886aaf75f04b887a30fcffb1
mateuszrosa <mateusz.rosa@gmail.com>
2020-06-29 12:35:31 +0200

    adding all values and displaying result

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const inputs = document.querySelectorAll('input[type="number"]');
-const sum = document.querySelector("span.font-bold");
+const result = document.querySelector("span.font-bold");
+let sum = 0;
 
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("change", (e) => {
-    sum.textContent = "";
+    sum = 0;
+    result.textContent = "";
     inputs.forEach((input) => {
       const price = input.parentNode.nextElementSibling.textContent.replace(
         /[^0-9]/g,
         ""
       );
       const value = input.value;
+      sum += price * value;
+      result.textContent = `${sum} zł`;
     });
   });
 }
