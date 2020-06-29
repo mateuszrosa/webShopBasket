3ef4653a9831ba600eef5f6f8af55ac0417f4e9c
mateuszrosa <mateusz.rosa@gmail.com>
2020-06-29 12:31:39 +0200

    getting datas from inputs

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const sum = document.querySelector("span.font-bold");
 
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("change", (e) => {
-    console.log(sum.textContent);
+    sum.textContent = "";
+    inputs.forEach((input) => {
+      const price = input.parentNode.nextElementSibling.textContent.replace(
+        /[^0-9]/g,
+        ""
+      );
+      const value = input.value;
+    });
   });
 }
